7c92886d0cf08796244982477efff2b77c5a4f56
Jonah Mallard <benjaminjmallard@gmail.com>
2020-04-21 23:22:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,10 @@ def line(katz_deli)
     i = 0 
     new_customer = []
     until i == katz_deli.length
-    
+    new_customer<< "#{i++}. #{katz_deli[i]}"
+    i += 1
   end
-  
+  puts "The line is currently: " << new_customer.join(" ")
+end
 end
     
