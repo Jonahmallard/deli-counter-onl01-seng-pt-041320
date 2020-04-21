6bdd592607b497d26b2e07eb34a70740573e3cf7
Jonah Mallard <benjaminjmallard@gmail.com>
2020-04-21 23:18:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,10 @@ kats_deli = []
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
+  else 
+    i = 0 
+    new_customer = []
+    until i == katz_deli.length
     
   end
   
